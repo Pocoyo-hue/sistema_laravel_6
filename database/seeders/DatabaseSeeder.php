581d900160cdc0a62229b12055ce137b9df66e00
pocoyo-hue <nicolashermozavillavicencio@gmail.com>
2024-10-11 16:20:35 -0500

    v4.2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Producto;
 use App\Models\User;
-use App\Models\Venta;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,18 +17,9 @@ class DatabaseSeeder extends Seeder
 
         // User::factory(10)->create();
 
-        $venta = new Venta();
-        $venta-> metodopago = "Efectivo";
-        $venta-> fechaventa = "29/09/2024";
-        $venta-> totalfactura = 200.10;
-        $venta->save();
+        $this->call(ProductoSeeder::class);
 
-        $venta2 = new Venta();
-        $venta2-> metodopago = "Tarjeta";
-        $venta2-> fechaventa = "29/09/2024";
-        $venta2-> totalfactura = 50.0;
-        $venta2->save();
-
+        
        //  User::factory()->create([
        //     'name' => 'Test User',
        //     'email' => 'test@example.com',
